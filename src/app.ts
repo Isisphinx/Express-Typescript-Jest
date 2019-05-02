b70b2529dccc79c85da05504b648a0e6238406e7
Thibaut Constant <thibaut.constant@isisphinx.com>
2019-05-02 19:22:41 +0200

    linting changes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (_, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, (): void => console.log(`Example app listening on port ${port}!`))
 
 console.log(hello('you'))
