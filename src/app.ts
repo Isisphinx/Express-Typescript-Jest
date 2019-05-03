63b7e438954a9aa5326b0ddfa2798c7fc86b6c8e
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2019-05-03 23:52:41 +0200

    lf

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (_, res) => res.send('Hello World!'))
-app.listen(port, (): void => console.log(`Example app listening on port ${port}!`))
+app.listen(
+  port,
+  (): void => console.log(`Example app listening on port ${port}!`),
+)
 
 console.log(hello('you'))
